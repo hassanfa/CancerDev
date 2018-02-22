81527cf5cad60cfb591b6f4a747dae867a4852ae
hassanfa <hassan.foroughi@gmail.com>
2018-02-22 11:05:18 +0100

    Path fixed, varidct installation added, and sbatch wrapper is now adding proper path

--- src/workflows/runSbatch.py
+++ src/workflows/runSbatch.py
@@ -19,29 +19,35 @@ from snakemake.utils import read_job_properties
 parser = argparse.ArgumentParser(description='Process script and dependecies.')
 parser.add_argument("dependencies", nargs="*", help="{{dependencies}} string given by snakemake\n")
 parser.add_argument("snakescript", help="Snakemake generated shell script with commands to execute snakemake rule\n")
-parser.add_argument("--config", help="Config file to read sbatch settings from.")
+parser.add_argument("--cluster_config", help="Config file to read sbatch settings from.")
+parser.add_argument("--sample_config", help="Config file to read sbatch settings from.")
 args = parser.parse_args()
-print(args)
 
 ## snakemake will generate a jobscript containing all the (shell) commands from your Snakefile. 
 ## https://snakemake.readthedocs.io/en/stable/snakefiles/rules.html?highlight=markdown%20header%20job%20properties#job-properties
-
-
-config = args.config
+sample_config = json.load(open(args.sample_config))
 jobscript = args.snakescript
 job_properties = read_job_properties(jobscript)
+
 # access property defined in the cluster configuration file (Snakemake >=3.6.0), cluster.json
 time = job_properties["cluster"]["time"]
 cpu = job_properties["cluster"]["n"]
 account_slurm = job_properties["cluster"]["account"]
 
-scriptpath='/home/hassan.foroughi/repo/CancerDev/src/workflows/script/'
+logpath = os.path.join(sample_config["analysis"]["analysis_dir"], sample_config["analysis"]["sample_id"], sample_config["analysis"]["log"])
+scriptpath = os.path.join(sample_config["analysis"]["analysis_dir"], sample_config["analysis"]["sample_id"], sample_config["analysis"]["script"])
+resultpath = os.path.join(sample_config["analysis"]["analysis_dir"], sample_config["analysis"]["sample_id"], sample_config["analysis"]["result"])
+
+os.system('mkdir -p ' + logpath)
+os.system('mkdir -p ' + scriptpath)
+os.system('mkdir -p ' + resultpath)
 os.system('cp ' + jobscript + ' ' + scriptpath)
 scriptname=jobscript.split("/")
 scriptname=scriptname[-1]
+jobscript = os.path.join(scriptpath, scriptname)
 
-output_log = scriptpath + scriptname +  ".out"
-error_log = scriptpath + scriptname +  ".err"
+output_log = logpath + scriptname + ".out"
+error_log = logpath + scriptname + ".err"
 cmdline = 'sbatch -A {account} -n {n} -t {time} --qos=low -o {output_log} -e {error_log}'.format(n=cpu, time=time, output_log=output_log, error_log=error_log, account=account_slurm)
 
 cmdline += " "
@@ -51,19 +57,16 @@ dependencies = args.dependencies
 if dependencies:
     cmdline += '--dependency=' + ','.join(["afterok:%s" % d for d in dependencies])
 
-cmdline += " "
-
 # the actual job
-cmdline += scriptpath + scriptname
+cmdline += " " + jobscript + " | cut -d' ' -f 4"
 
-cmdline += " | cut -d' ' -f 4"
-logpath='/home/hassan.foroughi/repo/CancerDev/src/workflows/logs/'
-os.system('ln -sf ' + output_log + ' ' + logpath) 
-os.system('ln -sf ' + error_log + ' ' + logpath) 
+#logpath='/home/hassan.foroughi/repo/CancerDev/src/workflows/logs/'
+#os.system('ln -sf ' + output_log + ' ' + logpath) 
+#os.system('ln -sf ' + error_log + ' ' + logpath) 
 
-f = open('helloworld.txt','a')
-f.write("cancer." + job_properties["rule"] + "." +  str(job_properties["jobid"]) + "\t" + job_properties["cluster"]["name"] + "\n")
-f.close()
+#f = open('helloworld.txt','a')
+#f.write("cancer." + job_properties["rule"] + "." +  str(job_properties["jobid"]) + "\t" + job_properties["cluster"]["name"] + "\n")
+#f.close()
 
 # call the command
-os.system(cmdline + "| cut -d' ' -f 4")
+os.system(cmdline)
